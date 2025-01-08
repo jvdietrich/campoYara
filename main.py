e40d75f449d9480a4a6757695fabeab2a352c95a
vergara <jose.dietrich@gmail.com>
2025-01-08 17:24:56 -0300

    atualizada imagem

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ aspersores_status = gerar_dados_aspersores()
 st.markdown(STYLE, unsafe_allow_html=True)
 
 # Caminho da imagem de fundo (use sua própria imagem ou URL)
-background_image_url = "https://via.placeholder.com/600x400.png?text=Fundo+dos+Aspersores"  # Substitua pelo caminho da sua imagem
+background_image_url = "https://izabeldietrich.com.br/wp-content/uploads/2024/12/campoH.png"  # Substitua pelo caminho da sua imagem
 
 # Criação do layout com a imagem de fundo e os círculos
 html_content = f"""
